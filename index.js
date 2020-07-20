94484ba5986a9be03f1381e49081f499f74b5fab
Paulahu <paulahu1995@gmail.com>
2020-07-20 20:25:38 +0800

    npm publish

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 import Button from './src/Button'
 import Icon from './src/Icon'
 import ButtonGroup from './src/Button-group'
+import Col from './src/Col'
+import Row from './src/Row'
+import Input  from './src/Input'
 
-export {Button, ButtonGroup, Icon}
+export {Button, ButtonGroup, Icon, Row, Input, Col}
